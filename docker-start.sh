56a1ce3dd2268556d7f7b57c2f2b29153cd841ec
Yunzhu Li <contact@yunzhu.li>
2017-02-20 02:43:02 -0500

    Update and rename docker script and config

--- docker-start.sh
+++ docker-start.sh
@@ -6,4 +6,4 @@ sed -i "s|var server_uri.*|var server_uri = '$SERVER_URI';|" /app/client/snake_c
 sed -i "s|var socket_io_path.*|var socket_io_path = '$SOCKET_IO_PATH';|" /app/client/snake_client.js
 
 nginx
-node /app/server/
+exec node /app/server/
